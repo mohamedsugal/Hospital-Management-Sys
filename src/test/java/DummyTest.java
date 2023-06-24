f0916ed5c9f903268fd669dfe9dfb8f3e7430385
Mohamed Sugal <sugaal@gmail.com>
2023-06-23 22:41:29 -0500

    Initial commit

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+    @Test
+    public void dummyTest() {
+        String hello = "hello";
+        Assertions.assertEquals("hello", hello);
+    }
+}
