dc2721d06c2c97b834a56fa0ddd66f1bbafa9ba8
Abdifatah Mohamed <mofaatah@gmail.com>
2023-06-25 17:45:25 -0500

    task1

--- src/main/java/org/example/Hospital.java
+++ src/main/java/org/example/Hospital.java
@@ -28,33 +28,53 @@ public class Hospital {
 
     // Task 1: Method that assigns doctors with nurses in a similar specialty
     public void assignNursesToDoctors() {
-        Map<String, List<String>> assignNurseToDoctor = new HashMap<>();
-
-        for (Doctor doctor : doctors) {
-            String doctorSpecialty = doctor.getSpecialty();
-            String doctorName = doctor.getName();
-
-            // List to store nurse names for each doctor
+        // Grab nurses name to their specialities without word "Nurse"
+        Map<String, List<String>> mapDoctorsSpecialtiesToTheirNames = mapDoctorsSpecialtiesToTheirNames(doctors);
+        Map<Doctor, List<Nurse>> assignNurseToDoctor = new HashMap<>();
+        for (Nurse nurse : nurses){
+            // This is nurses name
+            String nurseName = nurse.getName();
+            String specialty = nurse.getSpecialty();
+            // This is nurses specialty
+            String nurseSpecialty = specialty.substring(0, specialty.length() - 6);
             List<String> assignedNurses = new ArrayList<>();
-
-            for (Nurse nurse : nurses) {
-                String nurseName = nurse.getName();
-                String specialty = nurse.getSpecialty();
-                String nurseSpecialty = specialty.substring(0, specialty.length() - 6);
-
-                if (doctorSpecialty.equals(nurseSpecialty)) {
+            for(String doctors : mapDoctorsSpecialtiesToTheirNames.get(nurseSpecialty)){
+                String doctorName = doctors;
+                if(mapDoctorsSpecialtiesToTheirNames.get(nurseSpecialty).equals(nurseSpecialty)){
                     assignedNurses.add(nurseName);
+
                 }
+
+
             }
 
-            assignNurseToDoctor.put(doctorName, assignedNurses);
+//            assignNurseToDoctor.put(doctorName, assignedNurses);
+        }
+    }
+
+    // Method that map that maps doctor's specialities as a key and their names as a value of list.
+    private static Map<String, List<String>> mapDoctorsSpecialtiesToTheirNames(List<Doctor> doctors) {
+        Map<String, List<String>> mapDoctorsSpecialtiesToTheirNames = new HashMap<>();
+
+        for (Doctor doctor : doctors) {
+            String doctorName = doctor.getName();
+            String doctorSpecialty = doctor.getSpecialty();
+
+            // Check if the specialty is already present in the map
+            if (mapDoctorsSpecialtiesToTheirNames.containsKey(doctorSpecialty)) {
+                List<String> doctorNamesList = mapDoctorsSpecialtiesToTheirNames.get(doctorSpecialty);
+                doctorNamesList.add(doctorName);
+            } else {
+                List<String> doctorNamesList = new ArrayList<>();
+                doctorNamesList.add(doctorName);
+                mapDoctorsSpecialtiesToTheirNames.put(doctorSpecialty, doctorNamesList);
+            }
         }
 
+//        mapDoctorsSpecialtiesToTheirNames.forEach((doctorSpeciality, doctorNamesList) ->
+//                System.out.println("Specialty " + doctorSpeciality + " assigned to Doctors " + doctorNamesList));
 
-
-        assignNurseToDoctor.forEach((doctor, nursesList) ->
-                System.out.println("Doctor " + doctor + " assigned to Nurse " + nursesList));
-
+        return mapDoctorsSpecialtiesToTheirNames;
     }
 
 }
