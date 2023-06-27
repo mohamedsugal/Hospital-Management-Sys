ce97d90b10b67bc139c5a0b4954c9190b58d189e
Mohamed Sugal <sugaal@gmail.com>
2023-06-27 17:35:54 -0500

    test doctor not applicable

--- src/test/java/AssignNursesToDoctorsTest.java
+++ src/test/java/AssignNursesToDoctorsTest.java
@@ -2,6 +2,9 @@ import org.example.*;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,50 +16,33 @@ public class AssignNursesToDoctorsTest {
         Doctor doctor1 = new Doctor("Abdifatah Mohamed", 29, "General Medicine", 123, List.of("Fever"));
         Nurse nurse1 = new Nurse("Hui Wang", 37, "Oncology Nurse", 7, "LPN");
 
-        Map<Doctor, List<Nurse>> assignNurseToDoctor = hospital.assignNursesToDoctors(
+        Map<Doctor, List<Nurse>> actualMap = hospital.assignNursesToDoctors(
                 List.of(doctor1),
                 List.of(nurse1)
         );
 
-         /**
-              Abdifatah Mohamed's assigned nurse should be N/A
-              since it doesn't have a nurse who have a similar specialty of "General Medicine"
-          */
+        Map<Doctor, List<Nurse>> expectedMap = new LinkedHashMap<>();
+        expectedMap.put(doctor1, new ArrayList<>());
+        expectedMap.put(HospitalData.notApplicableDoctor(), List.of(nurse1));
 
-        Assert.assertEquals(0, assignNurseToDoctor.get(doctor1).size());
-        /**
-         Hui Wang's assigned nurse should be N/A
-         since it doesn't have a doctor who have a similar specialty of "Oncology"
-         */
-        if (assignNurseToDoctor.get(doctor1).contains(nurse1)) { // true
-            // nurse1 is assigned to doctor1, it will assert that "Assigned" is equal to "N/A"
-            // which will fail the test
-            Assert.assertEquals("Assigned", "N/A");
-        } else { // if false
-            // It will assert that "N/A" is equal to "N/A" which will pass the test
-            Assert.assertEquals(HospitalData.notApplicableDoctor().getSpecialty(), "N/A");
-        }
-
+        Assert.assertEquals(expectedMap.toString(), actualMap.toString());
     }
 
-        @Test
+    @Test
     public void doctorShouldHaveNursesAssignedTest() {
-            Hospital hospital = new Hospital();
-            Doctor doctor1 = new Doctor("Mohamed Sugal", 29, "Surgery", 123, List.of("Fever"));
-            Nurse nurse1 = new Nurse("Amy Lee", 29, "Surgery Nurse", 3, "PNCB");
+        Hospital hospital = new Hospital();
+        Doctor doctor1 = new Doctor("Mohamed Sugal", 29, "Surgery", 123, List.of("Fever"));
+        Nurse nurse1 = new Nurse("Amy Lee", 29, "Surgery Nurse", 3, "PNCB");
 
-
-            Map<Doctor, List<Nurse>> assignDoctorToNurse = hospital.assignNursesToDoctors(
-                    List.of(doctor1),
-                    List.of(nurse1)
-            );
-
-            /**
-             Mohamed Sugal's assigned nurse should be a doctor
-             since it has a nurse who have a similar specialty of "General Medicine"
-             */
-            Assert.assertEquals(1, assignDoctorToNurse.get(doctor1).size());
-
+        Map<Doctor, List<Nurse>> doctorsToNursesMap = hospital.assignNursesToDoctors(
+                List.of(doctor1),
+                List.of(nurse1)
+        );
+        // Doctor Mohamed Sugal should have 1 nurse assigned to him
+        Assert.assertEquals(1, doctorsToNursesMap.get(doctor1).size());
+        Assert.assertEquals("Amy Lee", doctorsToNursesMap.get(doctor1).get(0).getName());
+        Assert.assertEquals("Surgery Nurse", doctorsToNursesMap.get(doctor1).get(0).getSpecialty());
+        Assert.assertEquals("PNCB", doctorsToNursesMap.get(doctor1).get(0).getCertification());
     }
 
     @Test
@@ -65,16 +51,12 @@ public class AssignNursesToDoctorsTest {
         Doctor doctor1 = new Doctor("John Smith", 29, "Neurology", 123, List.of("Fever"));
         Nurse nurse1 = new Nurse("Nasra Cise", 29, "Gastroenterology Nurse", 3, "PNCB");
 
-
-        Map<Doctor, List<Nurse>> assignDoctorToNurse = hospital.assignNursesToDoctors(
+        Map<Doctor, List<Nurse>> doctorsToNursesMap = hospital.assignNursesToDoctors(
                 List.of(doctor1),
                 List.of(nurse1)
         );
-
-        /**
-         John Smith's assigned nurse should be a doctor
-         since it has a nurse who have a similar specialty of "General Medicine"
-         */
-        Assert.assertEquals(0, assignDoctorToNurse.get(doctor1).size());
+        // Doctor John Smith should not have nurses assigned to him
+        Assert.assertEquals(0, doctorsToNursesMap.get(doctor1).size());
+        Assert.assertTrue(doctorsToNursesMap.get(doctor1).isEmpty());
     }
 }
